278e33d3303681cab77cfc673511b0f5861bcd1c
gohitx <gohit21@gmail.com>
2026-03-20 16:13:18 -0500

    🎨 [feat] Add ATM game and time screens with navigation functionality for improved user experience

---
+++ src/focus/screens/atm-time/time.ts
@@ -0,0 +1,18 @@
+export const timeScreenContent = `
+<div style="
+	width: 100%;
+	height: 100vh;
+	position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+">
+	<button class="back-btn" type="button" data-back-to="search" style="position: absolute; top: 0; left: 0;">
+		<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+			<polyline points="15 18 9 12 15 6"></polyline>
+		</svg>
+		Back
+	</button>
+	Hello Time
+</div>
+`;
