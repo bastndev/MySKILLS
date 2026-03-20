8b99f31a5e3ffa0facb11dcc0a778f375f76a626
gohitx <gohit21@gmail.com>
2026-03-20 09:23:48 -0500

    ⚽️ [feat] Remove unused panel views (one-game, pomodoro, yt-list), their associated registrations, and the 'rene.helloWorld' command.

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-import { oneGameProvider, pomodoroProvider, ytListProvider } from './panels/panels';
 import { YouTubeMusicViewProvider } from './view/view';
 
 export function activate(context: vscode.ExtensionContext) {
@@ -7,29 +6,12 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "rene" is now active!');
 
 	context.subscriptions.push(
-		vscode.window.registerTreeDataProvider(
-			'rene-one-game-view',
-			oneGameProvider
-		),
-		vscode.window.registerTreeDataProvider(
-			'rene-pomodoro-view',
-			pomodoroProvider
-		),
-		vscode.window.registerTreeDataProvider(
-			'rene-yt-list-view',
-			ytListProvider
-		),
 		vscode.window.registerWebviewViewProvider(
 			'rene-yt-music-view',
 			new YouTubeMusicViewProvider(context.extensionUri),
 			{ webviewOptions: { retainContextWhenHidden: true } }
 		)
 	);
-
-	const disposable = vscode.commands.registerCommand('rene.helloWorld', () => {
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 // On deactivate
--- src/panels/panels.ts
+++
@@ -1,3 +0,0 @@
-export { oneGameProvider } from './one-game';
-export { pomodoroProvider } from './pomodoro';
-export { ytListProvider } from './yt-list';
